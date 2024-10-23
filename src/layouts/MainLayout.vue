4071869b012e91b64f7b787a88001961587121a4
philipheferreira <philipheferreira@gmail.com>
2024-10-23 15:47:55 -0300

    Feat: card aulas

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,7 +5,7 @@
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title>
-          Quasar App
+          Site Aulas
         </q-toolbar-title>
 
         <!--<div>Quasar v{{ $q.version }}</div>-->
@@ -15,7 +15,7 @@
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
         <q-item-label header>
-          Essential Links
+          Menu
         </q-item-label>
 
         <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
--- src/pages/videos.vue
+++ src/pages/videos.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    Isso é uma pagina de videos
+    <div>
+      <q-card class="my-card q-ma-md">
+
+        <q-video src="https://www.youtube.com/embed/iumsSe2783I" />
+
+        <q-card-section>
+          <div class="text-h6">Aula 01</div>
+          <div class="text-subtitle2">By Prof. Milton Oliveira</div>
+        </q-card-section>
+
+        <q-card-section>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </q-card-section>
+
+      </q-card>
+    </div>
   </div>
 
 </template>
@@ -11,4 +27,9 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.my-card {
+  width: 100%;
+  max-width: 300px;
+}
+</style>
