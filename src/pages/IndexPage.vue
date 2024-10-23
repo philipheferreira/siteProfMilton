9365a8cf035602ab46f7a10d13d160d4af0e098f
philipheferreira <philipheferreira@gmail.com>
2024-10-23 18:34:48 -0300

    Feat: Create TimeLine Component and add index page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,16 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <linhaDoTempo></linhaDoTempo>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-
+import linhaDoTempo from 'components/TimeLine.vue'
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    linhaDoTempo
+  }
 })
 </script>
