97c03004354ec645d9c82ecfcfba5e4428fc6b0d
philipheferreira <philipheferreira@gmail.com>
2024-10-25 14:17:33 -0300

    Feat: table aulas and timeline infos

--- src/components/TableTeach.vue
+++ src/components/TableTeach.vue
@@ -1,226 +1,151 @@
 <template>
   <div class="q-pa-md">
-    <q-table class="my-sticky-header-column-table" flat bordered title="Horário aulas" :rows="rows" :columns="columns"
-      row-key="name" virtual-scroll />
+    <q-markup-table>
+      <thead>
+        <tr>
+          <th class="text-left">Horarios / Dias da semana</th>
+          <th class="text-right">Segunda</th>
+          <th class="text-right">Terça</th>
+          <th class="text-right">Quarta</th>
+          <th class="text-right">Quinta</th>
+          <th class="text-right">Sexta</th>
+          <th class="text-right">Sabado</th>
+          <th class="text-right">Domingo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="text-left"> 08:00 </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+        </tr>
+        <tr>
+          <td class="text-left"> 09:00 </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+        </tr>
+        <tr>
+          <td class="text-left"> 10:00 </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+        </tr>
+        <tr>
+          <td class="text-left"> 11:00 </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+        </tr>
+        <tr>
+          <td class="text-left"> 14:00 </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+        </tr>
+        <tr>
+          <td class="text-left"> 15:00 </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+        </tr>
+        <tr>
+          <td class="text-left"> 16:00 </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+        </tr>
+        <tr>
+          <td class="text-left"> 17:00 </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+        </tr>
+        <tr>
+          <td class="text-left"> 18:00 </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+        </tr>
+        <tr>
+          <td class="text-left"> 19:00 </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+        </tr>
+        <tr>
+          <td class="text-left"> 20:00 </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+        </tr>
+        <tr>
+          <td class="text-left"> 21:00 </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+          <td class="text-right"> -- </td>
+        </tr>
+      </tbody>
+    </q-markup-table>
   </div>
 </template>
 
 <script>
-const columns = [
-  {
-    name: 'name',
-    required: true,
-    label: 'Horarios/Dias',
-    align: 'left',
-    field: row => row.name,
-    format: val => `${val}`,
-    sortable: true
-  },
-  { name: 'segundaFeira', align: 'center', label: 'Segunda', field: 'segunda', sortable: true },
-  { name: 'tercaFeira', align: 'center', label: 'Terça', field: 'terca', sortable: true },
-  { name: 'quartaFeira', align: 'center', label: 'Quarta', field: 'quarta' },
-  { name: 'quintaFeira', align: 'center', label: 'Quinta', field: 'quinta' },
-  { name: 'sabado', align: 'center', label: 'Sexta', field: 'sexta' },
-  { name: 'calcium', align: 'center', label: 'Sabado', field: 'sabado', sortable: true },
-  { name: 'iron', align: 'center', label: 'Domingo', field: 'domingo', sortable: true }
-]
-
-const rows = [
-  {
-    name: '08:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  },
-  {
-    name: '09:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  },
-  {
-    name: '10:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  },
-  {
-    name: '11:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  },
-  {
-    name: '12:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  },
-  {
-    name: '13:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  },
-  {
-    name: '14:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  },
-  {
-    name: '15:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  },
-  {
-    name: '16:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  },
-  {
-    name: '17:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  },
-  {
-    name: '18:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  },
-  {
-    name: '19:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  },
-  {
-    name: '20:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  },
-  {
-    name: '21:00',
-    segunda: '--',
-    terca: '--',
-    quarta: '--',
-    quinta: '--',
-    sexta: '--',
-    sabado: '--',
-    domingo: '--'
-  }
-]
 
 export default {
-  setup() {
-    return {
-      columns,
-      rows
-    }
-  }
+
 }
 </script>
 
 <style lang="sass">
-.my-sticky-header-column-table
-  /* height or max-height is important */
-  max-height: 400px
 
-  /* specifying max-width so the example can
-    highlight the sticky column on any browser window */
-  max-width: 900px
-  width: auto
-
-  td:first-child
-    /* bg color is important for td; just specify one */
-    background-color: #00b4ff
-
-  tr th
-    position: sticky
-    /* higher than z-index for td below */
-    z-index: 2
-    /* bg color is important; just specify one */
-    background: #00b4ff
-
-  /* this will be the loading indicator */
-  thead tr:last-child th
-    /* height of all previous header rows */
-    top: 48px
-    /* highest z-index */
-    z-index: 3
-  thead tr:first-child th
-    top: 0
-    z-index: 1
-  tr:first-child th:first-child
-    /* highest z-index */
-    z-index: 3
-
-  td:first-child
-    z-index: 1
-
-  td:first-child, th:first-child
-    position: sticky
-    left: 0
-
-  /* prevent scrolling behind sticky top row on focus */
-  tbody
-    /* height of all previous header rows */
-    scroll-margin-top: 48px
 </style>
--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -2,80 +2,74 @@
   <div class="q-px-lg q-py-md">
     <q-timeline :layout="layout" color="secondary">
       <q-timeline-entry heading>
-        Timeline heading
+        Linha do tempo Profissional
         <br>
         ({{ $q.screen.lt.sm ? 'Dense' : ($q.screen.lt.md ? 'Comfortable' : 'Loose') }} layout)
       </q-timeline-entry>
 
-      <q-timeline-entry title="Event Title" subtitle="February 22, 1986" side="left">
+      <q-timeline-entry title="Início da Graduação em Licenciatura" subtitle="01 de janeiro de 2015" side="left">
         <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
+          Início da Graduação em Licenciatura
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry title="Event Title" subtitle="February 21, 1986" side="right" icon="delete">
+      <q-timeline-entry title="Primeiras aulas Particulares" subtitle="02 de janeiro de 2016" side="right"
+        icon="delete">
         <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
+          Comecei a dar aulas particulares a alunos do ensino fundamental II
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry heading>November, 2017</q-timeline-entry>
+      <!--<q-timeline-entry heading>November, 2017</q-timeline-entry> -->
 
-      <q-timeline-entry title="Event Title" subtitle="February 22, 1986" side="left">
+      <q-timeline-entry title="Event Title" subtitle="02 de janeiro de 2017" side="left">
         <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
+          comecei a ministrar também aulas para alunos do ensino médio e participei de projeto de iniciação científic
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry title="Event Title" subtitle="February 22, 1986" side="right">
+      <q-timeline-entry title="Event Title" subtitle="02 de janeiro de 2018" side="right">
         <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
+          comecei a participar do projeto que viria a se chamar Análise e atendia alunos com objetivo de realizar a
+          prova
+          do ENEM e posteriormente trabalhamos preparando eles para procuras de concurso e mercado de trabalho e também
+          apresentei artigo do COPAM (Congresso Panamazonico realizado na UFPA).
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry title="Event Title" subtitle="February 22, 1986" side="left" color="orange" icon="done_all">
+      <q-timeline-entry title="Event Title" subtitle="02 de janeiro de 2019" side="left" color="orange" icon="done_all">
         <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
+          Diploma em Licenciatura plena em Matemática
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry title="Event Title" subtitle="February 22, 1986" side="right">
+      <q-timeline-entry title="Event Title" subtitle="02 de janeiro de 2020" side="right">
         <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
+          aperfeiçoamento pessoal em matéria de religião com cursos de extensão da faculdade católica
         </div>
       </q-timeline-entry>
 
-      <q-timeline-entry title="Event Title" subtitle="February 22, 1986" side="left">
+      <q-timeline-entry title="Event Title" subtitle="02 de janeiro de 2021" side="left">
         <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
+          Entendimento de que os problemas dos alunos passavam por não entender problemas matemáticos, traduzir para a
+          linguagem matemática e montar um plano de resolução no qual soubessem descrever o porquê.
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry title="Event Title" subtitle="02 de janeiro de 2022" side="right">
+        <div>
+          Conheci a EAM (Experiência da Aprendizagem Mediada) elaborada por Reuven Feuerstein onde ele mostra os
+          aspectos ambientais necessários para a educação e os métodos de mediação para ajudar o professor no auxilio
+          da autonomia do aluno, desenvolvimento cognitivo.
+        </div>
+      </q-timeline-entry>
+
+      <q-timeline-entry title="Event Title" subtitle="02 de janeiro de 2023" side="left">
+        <div>
+          Contato com a teoria RP (Resolução de Problemas) a qual deu uma resposta ao problema em que encontrei na
+          dificuldade dos meus alunos em conseguir aprender a matemática e mostrou como torna-lá relevante para a
+          vivência do aluno e um instrumento de desenvolvimento cognitivo, já que ela poderia reunir o que Reuven
+          Feuerstein trabalha como necessário no desenvolvimento do aluno e a neurociência confirma.
         </div>
       </q-timeline-entry>
     </q-timeline>
